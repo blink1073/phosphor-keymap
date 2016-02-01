f22b932f4d57e7a644df8da9500c71abd2a1c2b1
Steven Silvester <steven.silvester@ieee.org>
2016-02-01 13:16:46 -0600

    Clean up

--- example/webpack.conf.js
+++ example/webpack.conf.js
@@ -3,10 +3,5 @@ module.exports = {
   entry: './index.js',
   output: {
     filename: './bundle.js'
-  },
-  module: {
-    loaders: [
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
-    ]
   }
 }
--- test/webpack.conf.js
+++ test/webpack.conf.js
@@ -6,9 +6,4 @@ module.exports = {
     filename: './test/build/bundle.js'
   },
   bail: true,
-  module: {
-    loaders: [
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
-    ],
-  }
 }
